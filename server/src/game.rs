a288c5dea8ec3967d2065a3c743cb3d118789fd5
Malcolm Lockyer <segfault88@gmail.com>
2025-09-28 22:24:40 +1300

    try mpsc just for fun

--- server/src/game.rs
+++ server/src/game.rs
@@ -1,24 +1,35 @@
 use std::sync::Arc;
 use tokio::sync::RwLock;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel};
 use tokio::time::{Duration, interval};
 use tokio_util::sync::CancellationToken;
-use tracing::info;
+use tracing::{info, debug};
 
 const TICKS_PER_SECOND: u64 = 1;
 
 #[derive(Debug)]
 pub struct GameState {
-    player_count: usize,
-    next_player_id: usize,
+    player_count: u16,
+    next_player_id: u16,
+    send: UnboundedSender<GameAction>,
+    receive: UnboundedReceiver<GameAction>,
 }
 
 impl GameState {
     pub fn new() -> Self {
+        let (send, receive) = unbounded_channel::<GameAction>();
+
         GameState {
             player_count: 0,
             next_player_id: 1,
+            send,
+            receive,
         }
     }
+}
+
+enum GameAction {
+    Join { player_id: u16 },
 }
 
 pub struct Game {
@@ -63,7 +74,15 @@ impl Game {
     }
 
     pub async fn game_tick(&self) {
-        let lock = self.state.write().await;
+        let mut lock = self.state.write().await;
+
+        while let Ok(action) = lock.receive.try_recv() {
+            match action {
+                GameAction::Join { player_id } => {
+                    info!(player_id, "completing join")
+                }
+            }
+        }
 
         info!(lock.player_count, "tick");
     }
@@ -74,16 +93,19 @@ pub struct GameHandle {
 }
 
 impl GameHandle {
-    pub async fn player_count(&self) -> usize {
+    pub async fn player_count(&self) -> u16 {
         let lock = self.state.read().await;
         lock.player_count
     }
 
-    pub async fn add_player(&self) -> usize {
+    pub async fn add_player(&self) -> u16 {
         let mut lock = self.state.write().await;
         lock.player_count += 1;
         let player_id = lock.next_player_id;
         lock.next_player_id += 1;
+
+        lock.send.send(GameAction::Join {player_id}).unwrap();
+
         player_id
     }
 }
