a2fb1a33e1c7ac712b739a5115d1296670afc917
Malcolm Lockyer <segfault88@users.noreply.github.com>
2025-10-05 18:03:53 +1300

    draw into canvas from rust

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -6,18 +6,43 @@ pub fn start_game() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen(start)]
-fn run() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+fn run() {
+    let document = web_sys::window().expect("no global `window` exists").document().expect("should have a document on window");
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_text_content(Some("Hello from Rust!"));
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
 
-    body.append_child(&val)?;
+    context.begin_path();
 
-    Ok(())
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
 }
